ea7f43731e4698ddf0d54760cca246d3786507a3
quangdv <quang99dev@gmail.com>
2023-03-10 13:05:20 +0700

    initial commit

---
+++ src/configs/app.config.js
@@ -0,0 +1,10 @@
+const appConfig = {
+  apiPrefix: '/api',
+  authenticatedEntryPath: '/home',
+  unAuthenticatedEntryPath: '/sign-in',
+  tourPath: '/',
+  locale: 'en',
+  enableMock: true,
+};
+
+export default appConfig;
